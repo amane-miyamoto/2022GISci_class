9dc79024fedbdbed67d71080774774dad609ec93
naru-T <11214589+naru-T@users.noreply.github.com>
2022-04-11 14:19:50 +0900

    [edit] bookdown build works

--- geocompr-master/code/04-spatial-join.R
+++ geocompr-master/code/04-spatial-join.R
@@ -23,8 +23,11 @@ if(!exists("random_joined")) {
   
 }
 
+untibble = function(x) structure(x, class = setdiff(class(x), c("tbl_df", "tbl")))
+
 # summary(random_joined$name_long) # factors still there
 random_joined$name_long = as.character(random_joined$name_long)
+random_points <- untibble(random_points)
 
 jm0 = tm_shape(world) + tm_borders(lwd = 0.2) + tm_format("World")
 
@@ -34,21 +37,21 @@ jm1 = jm0 +
   tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
 
 jm2 = jm0 +
-  tm_shape(world_random, bbox = bb_world) +
+  tm_shape(untibble(world_random), bbox = bb_world) +
   tm_fill(col = "name_long", palette = "Dark2") +
   tm_layout(legend.show = FALSE)
   #tm_borders(col = "name_long", lwd = 4) + # issue with tmap: no variable border
   # tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
 
 jm3 = jm0 +
-  tm_shape(shp = random_joined, bbox = bb_world) +
+  tm_shape(shp = untibble(random_joined), bbox = bb_world) +
   tm_symbols(col = "name_long", shape = 4, border.lwd = 2, palette = "Dark2") +
   tm_layout(legend.show = FALSE)
 # +
 #   tm_layout(scale = 1, legend.bg.color = "white", legend.bg.alpha = 0.3, legend.position = c("right", "bottom"))
 
 jm4 = jm0 +
-  tm_shape(shp = random_joined, bbox = bb_world) +
+  tm_shape(shp = untibble(random_joined), bbox = bb_world) +
   tm_symbols(col = "name_long", shape = 4, border.lwd = 2, palette = "Dark2") +
   tm_layout(legend.only = TRUE)
 
